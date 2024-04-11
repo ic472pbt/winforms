d87421e4a1f68de62390848e261b944d6ea88c02
Pavel Tatarintsev <ic472pbt@ya.ru>
2024-04-11 20:33:51 +0400

    калькуляор уровня хронофизиологической адаптации

---
+++ DeSync/Form1.Designer.vb
@@ -0,0 +1,230 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.UI = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.AT1 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.AT2 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.AT3 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.AT4 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.K1 = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.K2 = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.K3 = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.K4 = New System.Windows.Forms.TextBox()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.M = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.D = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        resources.ApplyResources(Me.Label1, "Label1")
+        Me.Label1.Name = "Label1"
+        '
+        'Label2
+        '
+        resources.ApplyResources(Me.Label2, "Label2")
+        Me.Label2.Name = "Label2"
+        '
+        'UI
+        '
+        resources.ApplyResources(Me.UI, "UI")
+        Me.UI.Name = "UI"
+        '
+        'Label3
+        '
+        resources.ApplyResources(Me.Label3, "Label3")
+        Me.Label3.Name = "Label3"
+        '
+        'AT1
+        '
+        resources.ApplyResources(Me.AT1, "AT1")
+        Me.AT1.Name = "AT1"
+        '
+        'Label4
+        '
+        resources.ApplyResources(Me.Label4, "Label4")
+        Me.Label4.Name = "Label4"
+        '
+        'AT2
+        '
+        resources.ApplyResources(Me.AT2, "AT2")
+        Me.AT2.Name = "AT2"
+        '
+        'Label5
+        '
+        resources.ApplyResources(Me.Label5, "Label5")
+        Me.Label5.Name = "Label5"
+        '
+        'AT3
+        '
+        resources.ApplyResources(Me.AT3, "AT3")
+        Me.AT3.Name = "AT3"
+        '
+        'Label6
+        '
+        resources.ApplyResources(Me.Label6, "Label6")
+        Me.Label6.Name = "Label6"
+        '
+        'AT4
+        '
+        resources.ApplyResources(Me.AT4, "AT4")
+        Me.AT4.Name = "AT4"
+        '
+        'Label7
+        '
+        resources.ApplyResources(Me.Label7, "Label7")
+        Me.Label7.Name = "Label7"
+        '
+        'K1
+        '
+        resources.ApplyResources(Me.K1, "K1")
+        Me.K1.Name = "K1"
+        '
+        'Label8
+        '
+        resources.ApplyResources(Me.Label8, "Label8")
+        Me.Label8.Name = "Label8"
+        '
+        'K2
+        '
+        resources.ApplyResources(Me.K2, "K2")
+        Me.K2.Name = "K2"
+        '
+        'Label9
+        '
+        resources.ApplyResources(Me.Label9, "Label9")
+        Me.Label9.Name = "Label9"
+        '
+        'K3
+        '
+        resources.ApplyResources(Me.K3, "K3")
+        Me.K3.Name = "K3"
+        '
+        'Label10
+        '
+        resources.ApplyResources(Me.Label10, "Label10")
+        Me.Label10.Name = "Label10"
+        '
+        'K4
+        '
+        resources.ApplyResources(Me.K4, "K4")
+        Me.K4.Name = "K4"
+        '
+        'Label11
+        '
+        resources.ApplyResources(Me.Label11, "Label11")
+        Me.Label11.Name = "Label11"
+        '
+        'M
+        '
+        resources.ApplyResources(Me.M, "M")
+        Me.M.Name = "M"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.D)
+        resources.ApplyResources(Me.GroupBox1, "GroupBox1")
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.TabStop = False
+        '
+        'D
+        '
+        resources.ApplyResources(Me.D, "D")
+        Me.D.Name = "D"
+        '
+        'Form1
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.K4)
+        Me.Controls.Add(Me.AT4)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.K3)
+        Me.Controls.Add(Me.AT3)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.K2)
+        Me.Controls.Add(Me.AT2)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.K1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.AT1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.M)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.UI)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents UI As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents AT1 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents AT2 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents AT3 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents AT4 As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents K1 As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents K2 As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents K3 As TextBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents K4 As TextBox
+    Friend WithEvents Label11 As Label
+    Friend WithEvents M As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents D As Label
+End Class
---
+++ DeSync/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Function GetFloatValue(TB As TextBox) As Double
+        Return If(TB.Text.Length > 0, CDbl(TB.Text), 0.0)
+    End Function
+
+    Private Sub UI_TextChanged(sender As Object, e As EventArgs) Handles UI.TextChanged, M.TextChanged, AT1.TextChanged, AT2.TextChanged, AT3.TextChanged, AT4.TextChanged, K1.TextChanged, K2.TextChanged, K3.TextChanged, K4.TextChanged
+        Dim UIv As Single = If(UI.Text.Length > 0, CDbl(UI.Text), 0.0)
+        Dim At = {AT1, AT2, AT3, AT4}.Select(AddressOf GetFloatValue)
+        Dim K = {K1, K2, K3, K4}.Select(AddressOf GetFloatValue)
+        Dim Misc = {UI, M}.Select(AddressOf GetFloatValue)
+        D.Text = (Misc(0) * (At.Sum) / (K.Sum) * Misc(1) * 100.0).ToString("F4")
+    End Sub
+End Class
